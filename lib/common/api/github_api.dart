93ff2b6737dfe516a2b494cc5e97ec3bd4bc7753
lucinhu <1543498474@qq.com>
2023-02-06 18:17:53 +0800

    update requestLatestRelease()

--- lib/common/api/github_api.dart
+++ lib/common/api/github_api.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:dio/dio.dart';
 
 import '../models/github/github_releases_item.dart';
@@ -7,7 +9,8 @@ class GithubApi {
   static Future<GithubReleasesItemModel> requestLatestRelease() async {
     var response = await Dio().get(ApiConstants.githubLatestRelease,
         options: Options(headers: {
-          "Authorization": "token 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"
+          "Authorization": base64.decode(
+              "dG9rZW4gZ2hwX05ia0huNm9aRlRJN0ZyTzFPb1R4MkF3U21oTFN0OTBhN1lQVQ==")
         }));
 
     return GithubReleasesItemModel.fromJson(response.data);
