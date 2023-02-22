54f4c5bd72bef8ff4c5dd0d78e96b478915f672b
lucinhu <1543498474@qq.com>
2023-02-22 09:16:11 +0800

    upgrade dio; remove fanjiao_danmu package

--- lib/common/utils/my_dio.dart
+++ lib/common/utils/my_dio.dart
@@ -5,7 +5,6 @@ import 'package:cookie_jar/cookie_jar.dart';
 import 'package:dart_json_mapper/dart_json_mapper.dart';
 import 'package:dio/dio.dart';
 import 'package:dio_cookie_manager/dio_cookie_manager.dart';
-import 'package:dio_flutter_transformer2/dio_flutter_transformer2.dart';
 import 'package:path_provider/path_provider.dart';
 
 abstract class MyDio {
@@ -22,7 +21,8 @@ abstract class MyDio {
       cookieManager =
           CookieManager(PersistCookieJar(storage: FileStorage(cookiePath)));
     }
-    dio.transformer = FlutterTransformer();
+    dio.transformer = BackgroundTransformer();
+    dio.options.contentType = Headers.jsonContentType;
     dio.interceptors.add(cookieManager);
     try {
       dio.get(ApiConstants.bilibiliBase); //获取默认cookie
