f20174bf663489bb163cbac3444b4c68eec1184c
lucinhu <1543498474@qq.com>
2023-04-06 19:40:11 +0800

    feat: 添加评论图片分享

--- lib/pages/bili_video/widgets/reply/widgets/view_image.dart
+++ lib/pages/bili_video/widgets/reply/widgets/view_image.dart
@@ -3,6 +3,7 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 import 'package:photo_view/photo_view.dart';
+import 'package:share_plus/share_plus.dart';
 
 class ViewImage extends StatelessWidget {
   const ViewImage({super.key, required this.url});
@@ -13,11 +14,28 @@ class ViewImage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('查看图片'),
+        actions: [
+          PopupMenuButton(
+            itemBuilder: (context) => [
+              PopupMenuItem(
+                child: const Text('分享'),
+                onTap: () async {
+                  Share.shareXFiles([
+                    XFile((await CacheManager(Config(CacheKeys.replyImageKey))
+                            .getFileFromCache(url))!
+                        .file
+                        .path)
+                  ]);
+                },
+              )
+            ],
+          )
+        ],
       ),
       body: PhotoView(
         imageProvider: CachedNetworkImageProvider(url,
             cacheManager: CacheManager(Config(CacheKeys.replyImageKey))),
-        enableRotation: true,
+        // enableRotation: true,
         heroAttributes: PhotoViewHeroAttributes(tag: url),
       ),
     );
