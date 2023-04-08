16bad26c96125bd8b31af78fefa0e4174f94a969
lucinhu <1543498474@qq.com>
2023-04-08 22:42:50 +0800

    fix: 修复启动慢的问题

--- lib/common/utils/my_dio.dart
+++ lib/common/utils/my_dio.dart
@@ -1,12 +1,10 @@
 import 'dart:developer';
-import 'dart:io';
 
 import 'package:bili_you/common/api/api_constants.dart';
 import 'package:cookie_jar/cookie_jar.dart';
 import 'package:dio/dio.dart';
 import 'package:dio_cookie_manager/dio_cookie_manager.dart';
 import 'package:flutter/foundation.dart';
-import 'package:native_dio_adapter/native_dio_adapter.dart';
 import 'package:path_provider/path_provider.dart';
 
 abstract class MyDio {
@@ -24,17 +22,18 @@ abstract class MyDio {
       cookieManager =
           CookieManager(PersistCookieJar(storage: FileStorage(cookiePath)));
     }
-    if (Platform.isIOS || Platform.isMacOS || Platform.isAndroid) {
-      dio.httpClientAdapter = NativeAdapter();
-    }
     dio.interceptors.add(cookieManager);
     dio.options.headers = {'user-agent': ApiConstants.userAgent};
     dio.transformer = BackgroundTransformer();
     dio.options.contentType = Headers.jsonContentType;
-    try {
-      await dio.get(ApiConstants.bilibiliBase); //获取默认cookie
-    } catch (e) {
-      log("utils/my_dio, ${e.toString()}");
+    if ((await cookieManager.cookieJar
+            .loadForRequest(Uri.parse(ApiConstants.bilibiliBase)))
+        .isEmpty) {
+      try {
+        await dio.get(ApiConstants.bilibiliBase); //获取默认cookie
+      } catch (e) {
+        log("utils/my_dio, ${e.toString()}");
+      }
     }
   }
 }
